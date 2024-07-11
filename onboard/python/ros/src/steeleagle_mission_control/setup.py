7ba045007b6bd854a849a97ffcbb9b38926bdb01
xianglic <xianglic@andrew.cmu.edu>
2024-07-10 23:05:45 -0400

    hypervisor prototype

--- onboard/python/ros/src/steeleagle_mission_control/setup.py
+++ onboard/python/ros/src/steeleagle_mission_control/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'hypervisor = steeleagle_mission_control.hypervisor:main'
         ],
     },
 )
